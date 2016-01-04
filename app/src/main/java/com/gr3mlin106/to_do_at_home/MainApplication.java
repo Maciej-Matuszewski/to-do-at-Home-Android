afdc69b6e7f6769f0b52573e2400156b1ee56ef3
Gr3mlin106 <maciej.matuszewski@me.com>
2016-01-04 01:05:30 +0100

    add fabric

--- app/src/main/java/com/gr3mlin106/to_do_at_home/MainApplication.java
+++ app/src/main/java/com/gr3mlin106/to_do_at_home/MainApplication.java
@@ -6,12 +6,14 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.crashlytics.android.Crashlytics;
 import com.parse.ConfigCallback;
 import com.parse.Parse;
 import com.parse.ParseConfig;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.ParseUser;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by gr3mlin106 on 27.12.15.
@@ -31,6 +33,7 @@ public class MainApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
 
         Parse.enableLocalDatastore(this);
         Parse.initialize(this);
